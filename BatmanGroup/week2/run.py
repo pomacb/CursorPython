8b1f8fa32bf702da86129166ee36888a6102c927
Roman Zhovna <zhovna.roman@gmail.com>
2018-10-02 20:55:42 +0300

    task1 solved

--- BatmanGroup/week2/run.py
+++ BatmanGroup/week2/run.py
@@ -12,10 +12,26 @@ members = [
   {'age': 83, 'name': 'Homer'}
 ]
 
+
+def upmembers(l: list) -> list:
+    l = [{k: (v.upper() if k == 'name' else v) for (k, v) in d.items()} for d in members]
+    return l
+
+
+# def upmembers2(l: list) -> list:
+#     l = [dict(zip(d.keys(), list(map(lambda d: d['name'].upper(), members)))) for d in members]
+#     return l
+
+
 if __name__ == "__main__":
-    print(list(map(lambda x: x['name'].upper(), members)))
-  #  print([me for x in members])
-    for i in members:
-        for key in i.items():
-            i['name'] = i['name'].upper()
-    print(members)
+    print(upmembers(members))
+
+
+
+
+    # print(list(map(lambda x: x['name'].upper(), members)))
+    # for i in members:
+    #     for key in i.items():
+    #         i['name'] = i['name'].upper()
+    # print(members)
+
---
+++ BatmanGroup/week3/lesson1.py
@@ -0,0 +1,9 @@
+from datetime import datetime, timedelta
+
+
+
+print(datetime.now())
+print(datetime.now() + timedelta(hours=1)) # added 1 hour to current time
+print(datetime.utcnow())
+
+
