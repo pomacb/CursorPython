5af65abc06169ba139e4cbc990656673e437e8b7
Zhovna Roman <zhovna.roman@gmail.com>
2018-10-12 01:02:04 +0300

    [week3|lesson2] initial

---
+++ BatmanGroup/week3/lesson2/run.py
@@ -0,0 +1,9 @@
+from utils.task1 import Developer, Python, Ruby, Java
+
+dev1 = Developer(4, 'Boby')
+print(dev1.about())
+print(dev1.write_code())
+
+dev2 = Python(6, 'Roman')
+print(dev2.about())
+print(dev2.write_code())
---
+++ BatmanGroup/week3/lesson2/utils/task1.py
@@ -0,0 +1,42 @@
+class Developer():
+    def __init__(self, years_experience: int, name: str, language=''):
+        self.years_experience = years_experience
+        self.name = name
+
+    def about(self):
+        if self.years_experience <= 3:
+            return f"My name is {self.name} and I am Junior Developer."
+        elif self.years_experience <= 5:
+            return f"My name is {self.name} and I am Middle Developer."
+        else:
+            return f"My name is {self.name} and I am a Senior Developer."
+
+    def write_code(self):
+        return "I am a developer and I write code"
+
+
+class Python(Developer):
+    def __init__(self, years_experience, name, language='Python'):
+        Developer.__init__(self, years_experience, name)
+        self.language = language
+
+    def write_code(self):
+        return f"I use {self.language} to write code"
+
+
+class Ruby(Developer):
+    def __init__(self, years_experience, name, language='Ruby'):
+        Developer.__init__(self, years_experience, name)
+        self.language = language
+
+    def write_code(self):
+        return f"I use {self.language} to write code"
+
+
+class Java(Developer):
+    def __init__(self, years_experience, name, language='Java'):
+        Developer.__init__(self, years_experience, name)
+        self.language = language
+
+    def write_code(self):
+        return f"I use {self.language} to write code"
