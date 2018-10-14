49393c522547e7e8533d7ba4541320fb63a76411
Zhovna Roman <zhovna.roman@gmail.com>
2018-10-14 14:46:56 +0300

    modified task3

--- BatmanGroup/week3/lesson2/utils/task3.py
+++ BatmanGroup/week3/lesson2/utils/task3.py
@@ -1,13 +1,21 @@
-class Employees():
+class Employees:
 
-    def __index__(self):
-        self.employee_list = []
+    employee_list = []
 
-    def add_employee(self, new_emp):
-        self.employee_list.append(new_emp)
+    def __add__(self, empl2add: object):
+        if empl2add.years_experience >= 3:
+            self.employee_list.append(empl2add)
+            print(f"Employee {empl2add.name} successfully hired")
+        else:
+            print(f"Employee {empl2add.name} wasn't hired")
+            print(f"Please, try again in {3 - empl2add.years_experience} year(s)")
+        return self
 
-    def __str__(self):
-        return f"{self.employee_list}"
+    def print_employee(self):
+        sorted_employee = sorted(self.employee_list, key=lambda x: x.years_experience)
+        for empl in sorted_employee:
+            print(f"{empl.name} - {empl.years_experience} years, {empl.language}")
+
 
     def fire_employee(self):
         pass
