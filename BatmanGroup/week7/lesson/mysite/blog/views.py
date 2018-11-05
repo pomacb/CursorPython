76105971f30a6fc8223d7299b2f75607a8e13e5d
Zhovna Roman <zhovna.roman@gmail.com>
2018-11-05 23:40:11 +0200

    [week7|lesson2]

---
+++ BatmanGroup/week7/lesson/mysite/blog/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from blog.models import Article
+from django.shortcuts import render
+
+
+def index(request):
+    articles = Article.objects.all()
+    return render(request, 'index.html', {'articles': articles})
+
+
+def detail(request, article_id):
+    article = Article.objects.get(id=article_id)
+    return render(request, 'detail.html', {'article': article})
